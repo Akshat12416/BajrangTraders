391500ede92ebb5dff69e7649545817b188fbe0d
Akshat12416 <akshatgour35@gmail.com>
2026-07-15 03:22:19 +0530

    feat: add billedOrders state and management logic to orderStore

--- mobile/src/store/orderStore.js
+++ mobile/src/store/orderStore.js
@@ -6,17 +6,20 @@ export const useOrderStore = create(
   persist(
     (set, get) => ({
       pendingOrders: [],
+      billedOrders: [],
       addOrder: (order) => set({ 
         pendingOrders: [order, ...get().pendingOrders] 
       }),
       removeOrder: (id) => set({ 
         pendingOrders: get().pendingOrders.filter(o => o.id !== id) 
       }),
-      clearOrders: () => set({ pendingOrders: [] })
+      setBilledOrders: (orders) => set({ billedOrders: orders }),
+      clearOrders: () => set({ pendingOrders: [], billedOrders: [] })
     }),
     {
       name: 'pending-orders-storage',
       storage: createJSONStorage(() => AsyncStorage),
+      partialize: (state) => ({ pendingOrders: state.pendingOrders }), // Don't persist billed orders
     }
   )
 );
