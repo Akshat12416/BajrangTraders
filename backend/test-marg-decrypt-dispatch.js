bfd412649f62cad6d397084af0eb7d02a171805b
Akshat12416 <akshatgour35@gmail.com>
2026-07-18 19:40:25 +0530

    chore: remove test scripts and initialize .gitignore in backend

--- backend/test-marg-decrypt-dispatch.js
+++
@@ -1,9 +0,0 @@
-const { decryptMargResponseToJson } = require('./utils/margCrypto');
-const cipherText = "LRG0Ak4gga1wouR4YKn2qfWJHiCjN3Rnlmi2lq22MDlj1W1edUsYErw+OmnBGxa3IstvbYdE4zWUJA+lM/Twf6hOeD5E+oq3cedNpY3gZo9JeVf8aLeuH4cj/oIAFz7Ox7GXIFknjCTMGncGXFDZnrUAJHg62yLajBQxOxEPJbueqs8MdtOzNnUlwWye4OkMLSe4DibPD5YgfpM7Lv9uOrChzmnfkaccTfpNGmZnQjc=";
-const key = "LGWSIDNMXJ2Q";
-try {
-  const result = decryptMargResponseToJson(cipherText, key);
-  console.log(result);
-} catch (e) {
-  console.log("Error:", e.message);
-}
--- backend/test-marg-decrypt-strip.js
+++
@@ -1,9 +0,0 @@
-const { decryptMargResponseToJson } = require('./utils/margCrypto');
-const cipherText = "WSS2pJYmZOsZJVtVJwSWJJKZCl5AYUKS1KVdJR8k0tLk5MTwWJlebkKBRX5iUr5GTmZpYopFYkp6ampKboKdXWAgA=";
-const key = "LGWSIDNMXJ2Q";
-try {
-  const result = decryptMargResponseToJson(cipherText, key);
-  console.log(result);
-} catch (e) {
-  console.log("Error:", e.message);
-}
